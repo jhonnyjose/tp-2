3f021e638c40c413c2435c514c32138df9c7a101
jhonnyjose <jhonnydiaz31@gmail.com>
2021-06-24 09:50:14 -0300

    php

--- api/procesargracias.php
+++ api/procesargracias.php
@@ -1,5 +1,13 @@
 <?php
-header('content-type: application/json');
+
+
+
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$enviado = 'gracias.html';
+
+
+
 
 $nombre = $_POST['icon_prefix'];
 $correo = $_POST['email'];
@@ -8,7 +16,7 @@ $mensaje = $_POST['textarea1'];
 $subject = 'mensaje app';
 
 $enviaPara = 'jhonny.diaz@davinci.edu.ar';
-$enviado="../gracias.html";
+
 
 
 $textoCorreo = '';
@@ -25,5 +33,5 @@ $mail_headers .= "Content-type: text/html; charset=utf-8\r\n";
 $mail_headers .= 'From: ' . $correo . "\r\n";
 @mail($enviaPara, $subject, $textoCorreo, $mail_headers);
 
-header("Location: $enviado");
+header("Location: http://$host$uri/$enviado")
 ?>
